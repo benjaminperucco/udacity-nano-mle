99212438642619aeb5689008b954b421d6a2216b
EC2 Default User <ec2-user@ip-172-16-75-91.ec2.internal>
2020-12-20 19:31:21 +0000

    feat: Add knn model from scikit-learn
    Former-commit-id: c5af92b53f3c30ffef8d158a76a6b46ce652425c
    Former-commit-id: 5e655d271061123fb9610d4a603b76d2696bb831

--- 6 Tests/1 scikit-learn/source/train.py
+++ 6 Tests/1 scikit-learn/source/train.py
@@ -4,14 +4,18 @@ import joblib
 import os
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
 
     # hyperparameters for scikit-learn model
-    parser.add_argument('--max_leaf_nodes', type=int, default=-1)
-
+    parser.add_argument('--param_cv', type=int, default=5) # cross validation fold
+    parser.add_argument('--param_n_neighbors', type=int, default=5) # number of neighbors
+    parser.add_argument('--param_weights', type=str, default='uniform') # weight function used in prediction
+    parser.add_argument('--param_p', type=int, default=2) # power parameter for the Minkowski metric
+    
     # SageMaker specific arguments. defaults are set in the environment variables.
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
@@ -20,7 +24,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # take the set of files and read them all into a single pandas dataframe
-    input_files = [ os.path.join(args.train, file) for file in os.listdir(args.train)]
+    input_files = [os.path.join(args.train, file) for file in os.listdir(args.train)]
     if len(input_files) == 0:
         raise ValueError(('There are no files in {}.\n' +
                           'This usually indicates that the channel ({}) was incorrectly specified,\n' +
@@ -30,21 +34,26 @@ if __name__ == '__main__':
     raw_data = [pd.read_csv(file, header=None, engine="python") for file in input_files]
     train_data = pd.concat(raw_data)
 
-    # labels are in the first column
+    # labels are in the first column, features in rest columns
     train_y = train_data.iloc[:, 0]
     train_X = train_data.iloc[:, 1:]
 
-    # Here we support a single hyperparameter, 'max_leaf_nodes'. Note that you can add as many
-    # as your training my require in the ArgumentParser above.
-    max_leaf_nodes = args.max_leaf_nodes
+    # scikit-learn usual model definition
+    clf = KNeighborsClassifier(n_neighbors=args.param_n_neighbors, weights=args.param_weights, p=args.param_p)
+    
+    # calculate accuracy based on cross validation
+    accuracy_scores = cross_val_score(clf, train_X, train_y, cv=args.param_cv)
+    
+    # fit on full training data
+    clf.fit(train_X, train_y)
+    
+    # print out accuracy from cross validation
+    print('use {}-fold cross validation score'.format(args.param_cv))
+    print('accuracy: {}'.format(accuracy_scores.mean()))
 
-    # Now use scikit-learn's decision tree classifier to train the model.
-    clf = tree.DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes)
-    clf = clf.fit(train_X, train_y)
-
-    # Print the coefficients of the trained classifier, and save the coefficients
+    # dump model for use in model_fn function for predictions
     joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
-
+    
 
 def model_fn(model_dir):
     """Deserialized and return fitted model
@@ -53,3 +62,4 @@ def model_fn(model_dir):
     """
     clf = joblib.load(os.path.join(model_dir, "model.joblib"))
     return clf
+
