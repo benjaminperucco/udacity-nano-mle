cce989f21e732ea68acdf2f44a11b5b22f691c0f
Benjamin Perucco <benjamin.perucco@gmail.com>
2020-12-23 16:21:46 +0100

    feat: Use spaces not tabs

--- 2 Software Engineering Fundamentals/Source/helper.py
+++ 2 Software Engineering Fundamentals/Source/helper.py
@@ -1,32 +1,32 @@
 def import_kaggle_data(data_set, extract_zip=False):
-	"""
-	Download a data_set from personal kaggle account.
+    """
+    Download a data_set from personal kaggle account.
 
-	Args:
-		data_set: (string) name of dataset
-		extract_zip: (boolean) if dataset is stored in zip file on kaggle,
-		extract locally
+    Args:
+        data_set: (string) name of dataset
+        extract_zip: (boolean) if dataset is stored in zip file on kaggle,
+        extract locally
 
-	Returns:
-		Downloaded or extracted dataset is available on file system
+    Returns:
+        Downloaded or extracted dataset is available on file system
 
-	"""
+    """
 
-	from kaggle.api.kaggle_api_extended import KaggleApi
-	from zipfile import ZipFile
+    from kaggle.api.kaggle_api_extended import KaggleApi
+    from zipfile import ZipFile
 
-	print('Download {} from kaggle.com...'.format(data_set), end='')
-	api = KaggleApi()
-	api.authenticate()
-	api.dataset_download_file(
-		dataset='benjaminperucco/udacitydata',
-		file_name=data_set,
-		force=True
-	)
+    print('Download {} from kaggle.com...'.format(data_set), end='')
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_file(
+        dataset='benjaminperucco/udacitydata',
+        file_name=data_set,
+        force=True
+    )
 
-	if extract_zip:
-		zf = ZipFile(data_set + '.zip')
-		zf.extractall()
-		zf.close()
+    if extract_zip:
+        zf = ZipFile(data_set + '.zip')
+        zf.extractall()
+        zf.close()
 
-	print('done')
+    print('done')
