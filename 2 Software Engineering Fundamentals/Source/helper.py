79c7c35541123e64ca65f2c20b49a5762a4338ea
Benjamin Perucco <benjamin.perucco@gmail.com>
2020-12-23 16:10:27 +0100

    feat: Update description in helper file

--- 2 Software Engineering Fundamentals/Source/helper.py
+++ 2 Software Engineering Fundamentals/Source/helper.py
@@ -1,4 +1,16 @@
 def import_kaggle_data(data_set, extract_zip=False):
+	"""
+	Download a data_set from personal kaggle account.
+
+	Args:
+		data_set: (string) name of dataset
+		extract_zip: (boolean) if dataset is stored in zip file on kaggle,
+		extract locally
+
+	Returns:
+		Downloaded or extracted dataset is available on file system
+
+	"""
 
 	from kaggle.api.kaggle_api_extended import KaggleApi
 	from zipfile import ZipFile
